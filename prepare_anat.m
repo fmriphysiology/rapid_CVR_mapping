50743a5542423f1bb054e689adc919f687dab013
Nic Blockley <nicholas.blockley@ndcn.ox.ac.uk>
2016-03-16 14:27:25 +0000

    Updated prepare_anat.* so that FAST doesn't output binarised maps and tissue PVE mask are added to bids.mat

--- prepare_anat.m
+++ prepare_anat.m
@@ -3,4 +3,7 @@ function bids=prepare_anat(bids,subj);
 	status=system([bids(subj).dir 'derivatives/code/prepare_anat.sh ' bids(subj).dir ' ' bids(subj).name]);
 	if status==0
 		bids(subj).anat.bet=[bids(subj).dir 'derivatives/' bids(subj).name '/anat/' bids(subj).name '_T1w_brain'];
+		bids(subj).anat.csf=[bids(subj).dir 'derivatives/' bids(subj).name '/anat/' bids(subj).name '_T1w_brain_pve_0'];
+		bids(subj).anat.gm=[bids(subj).dir 'derivatives/' bids(subj).name '/anat/' bids(subj).name '_T1w_brain_pve_1'];
+		bids(subj).anat.wm=[bids(subj).dir 'derivatives/' bids(subj).name '/anat/' bids(subj).name '_T1w_brain_pve_2'];
 	end
--- prepare_anat.sh
+++ prepare_anat.sh
@@ -16,5 +16,5 @@
 $FSLDIR/bin/bet $1/$2/anat/$2_T1w $1/derivatives/$2/anat/$2_T1w_brain -f 0.5 -g 0
 
 # fast segment anatomical
-$FSLDIR/bin/fast -t 1 -n 3 -H 0.1 -I 4 -l 20.0 -g -o $1/derivatives/$2/anat/$2_T1w_brain $1/derivatives/$2/anat/$2_T1w_brain
+$FSLDIR/bin/fast -t 1 -n 3 -H 0.1 -I 4 -l 20.0 -o $1/derivatives/$2/anat/$2_T1w_brain $1/derivatives/$2/anat/$2_T1w_brain
 
